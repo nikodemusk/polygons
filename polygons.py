3139739d56a16915b064137fe5b75e3ad68955b9
Nikodemus Karlsson <nikodemusk@users.noreply.github.com>
2020-04-17 18:56:53 +0200

    Update polygons.py

--- polygons.py
+++ polygons.py
@@ -1,3 +1,8 @@
+# This Python 3 script creates a 3x3 chart of nested and twisted regular polygons.
+# Needs matplotlib and numpy.
+# Copyright Nikodemus Karlsson.
+# This work is released under CC-BY license.
+
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.patches as mpatches
@@ -6,44 +11,62 @@ plt.style.use("dark_background")
 plt.rcParams["mathtext.fontset"] = "cm"
 plt.rcParams['font.family'] = 'serif'
 
+### This parameters can easily be changed ###
+monochrome = False # If True, the chart is produced in light colors and the polygons in gray scale.
+saveExtents = False # Save each polygon plot to a png file?
+saveFigure = False # Save all chart to png file?
+showTextLabels = True # With or without text telling the number of sides and angle?
+fontSize = 22 # Obvious
+subpol = 100 # Number of turned polygons inside each other
+nv = [3, 4, 8] # Number of vertices in each row
+rotation = [[0, 2, 40], [0, 2, 15], [0, 2, 22.5]] # Rotation in degrees for inside polygon with respect to the closest outside one
+lineWidth = 0.5 # Obvious
+### End easily changeable parameters ###
+
 xlimit = (-0.8, 0.8)
 ylimit = (-0.8, 0.8)
-x, y = 0, 0 # center
-subpol = 100
-nv = [3, 4, 8] # Number of vertices in each row
-color = np.linspace(0, .9, subpol)
-lineWidth = 0.75
+x, y = 0, 0 # center of each polygon
+color = np.linspace(0, .9, subpol) # Need for color variation
 
-showTextLabels = True
-fontSize = 22
+if monochrome:
+    plt.style.use("default")
+    plt.rcParams["mathtext.fontset"] = "cm"
+    plt.rcParams['font.family'] = 'serif'
 
-fig, ax = plt.subplots(3, 3, figsize=(20,20))
-fig.set_dpi(100)
+fig, ax = plt.subplots(3, 3, # 3 rows and 3 columns
+                       figsize=(20,20), 
+                       dpi=100)
 
 # Create row 0
 row = 0
 # Outer loop, 3 columns. Each columns consists of triangles turned
 # in different angles.
 for col in range(0, 3):
-    rot = [0, 2, 40] # Rotation in each column
+    rot = rotation[row]
     
     # Inner loop, operates on one column. Creates the turned triangles
     # in different sizes.
     for j in range(0, subpol):
+        if monochrome:
+            edgeColor = [0,0,0, 1/((j+1)**(0.5))]
+        else:
+            edgeColor = [0.8, color[j], 0.1, 1/((j+2)**(0.1))]
+            
         polyParams = {'facecolor':'none',                                                                                 
-          'edgecolor':[0.8, color[j], 0.1, 1/((j+2)**(0.1))],
+          'edgecolor':edgeColor,
           'linewidth':lineWidth,
           'radius': 1/((j+2)**0.4),
           'numVertices': nv[row],
           'xy': (x, y),
-          'orientation': 0}
+          'orientation': 0} # I use rotate_deg_around() below, rather than orientation parameter
         
         rotationParams = {'degrees':j*rot[col]}
+        
         textParams = {
                       'verticalalignment': 'center',
                       'horizontalalignment': 'center',
                       'fontsize': fontSize,
-                      'transform': ax[row, col].transAxes}
+                      'transform': ax[row, col].transAxes} # Relative coordinates 0 - 1
         
         polygon = mpatches.RegularPolygon(**polyParams)
         transform = mpl.transforms.Affine2D().rotate_deg_around(x, y, **rotationParams)
@@ -58,24 +81,30 @@ for col in range(0, 3):
     if showTextLabels:
         plt.text(0.5, 0.1, f'$n={nv[row]}$, $\\alpha = {rot[col]}^\degree$', **textParams)
         
-    extent = ax[row, col].get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-    if showTextLabels:
-        fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent)
-    else:
-        fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent)
+    if saveExtents:
+        extent = ax[row, col].get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        if showTextLabels:
+            fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent)
+        else:
+            fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent)
         
 # Create row 1
 row = 1
 # Outer loop, 3 columns. Each columns consists of triangles turned
 # in different angles.
 for col in range(0, 3):
-    rot = [0, 2, 15] # Rotation in each column
+    rot = rotation[row]
     
     # Inner loop, operates on one column. Creates the turned triangles
     # in different sizes.
     for j in range(0, subpol):
+        if monochrome:
+            edgeColor = [0,0,0, 1/((j+1)**(0.5))]
+        else:
+            edgeColor = [0.2, 1, color[j], 1/((j+2)**(0.1))]
+            
         polyParams = {'facecolor':'none',                                                                                 
-          'edgecolor':[0.2, 1, color[j], 1/((j+2)**(0.1))],
+          'edgecolor':edgeColor,
           'linewidth':lineWidth,
           'radius': 1/((j+2)**0.4),
           'numVertices': nv[row],
@@ -83,6 +112,7 @@ for col in range(0, 3):
           'orientation': 0}
         
         rotationParams = {'degrees':j*rot[col]}
+        
         textParams = {
                       'verticalalignment': 'center',
                       'horizontalalignment': 'center',
@@ -102,24 +132,30 @@ for col in range(0, 3):
     if showTextLabels:
         ax[row, col].text(0.5, -0.025, f'$n={nv[row]}$, $\\alpha = {rot[col]}^\degree$', **textParams)
 
-    extent = ax[row, col].get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-    if showTextLabels:
-        fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent.expanded(1.1, 1.2))
-    else:
-        fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent)
+    if saveExtents:
+        extent = ax[row, col].get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        if showTextLabels:
+            fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent.expanded(1.1, 1.2))
+        else:
+            fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent)
 
 # Create row 2
 row = 2
 # Outer loop, 3 columns. Each columns consists of triangles turned
 # in different angles.
 for col in range(0, 3):
-    rot = [0, 2, 22.5] # Rotation in each column
+    rot = rotation[row]
     
     # Inner loop, operates on one column. Creates the turned triangles
     # in different sizes.
     for j in range(0, subpol):
+        if monochrome:
+            edgeColor = [0,0,0, 1/((j+1)**(0.5))]
+        else:
+            edgeColor = [color[j], 0.8, 0.9, 1/((j+2)**(0.1))]
+            
         polyParams = {'facecolor':'none',                                                                                 
-          'edgecolor':[color[j], 0.8, 0.9, 1/((j+2)**(0.1))],
+          'edgecolor':edgeColor,
           'linewidth':lineWidth,
           'radius': 1/((j+2)**0.4),
           'numVertices': nv[row],
@@ -127,6 +163,7 @@ for col in range(0, 3):
           'orientation': 0}
         
         rotationParams = {'degrees':j*rot[col]}
+        
         textParams = {
                       'verticalalignment': 'center',
                       'horizontalalignment': 'center',
@@ -146,11 +183,14 @@ for col in range(0, 3):
     if showTextLabels:
         ax[row, col].text(0.5, -0.025, f'$n={nv[row]}$, $\\alpha = {rot[col]}^\degree$', **textParams)
     
-    extent = ax[row, col].get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-    if showTextLabels:
-        fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent.expanded(1.1, 1.2))
-    else:
-        fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent)
+    if saveExtents:
+        extent = ax[row, col].get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        if showTextLabels:
+            fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent.expanded(1.1, 1.2))
+        else:
+            fig.savefig(f'polygon_{nv[row]}_{col}.png', bbox_inches=extent)
 
-fig.savefig('full.png', bbox_inches='tight')
+if saveFigure:
+    fig.savefig('polygons.png', bbox_inches='tight')
+
 plt.show()
